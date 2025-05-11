96bc006f6932f153e4ec4023a2add41710d9b1ec
Carla Berón <alracnoreb@gmail.com>
2025-05-10 22:13:38 -0300

    ejemplo de uso

--- src/main/java/ejercicio3/EjemploDeUsoDelReporte.java
+++ src/main/java/ejercicio3/EjemploDeUsoDelReporte.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 public class EjemploDeUsoDelReporte {
     public static void main(String[] args) {
-
-//        TipoDeGasto tipoDeGasto = TipoDeGasto.DESAYUNO;
-//        int monto =1000;
-//        Gasto gasto = new Gasto(tipoDeGasto, monto);
-//        var reporte = new ReporteDeGastos();
-//        String imprimirReporte = reporte.imprimir(List.of(gasto));
-//        System.out.println(imprimirReporte);
+        var reporte = new ReporteDeGastos(List.of(new Desayuno(1000)));
+        String reporteImpreso = reporte.generarReporte();
+        System.out.println(reporteImpreso);
     }
 }
